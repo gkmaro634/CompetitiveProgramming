87a764c53b91ad15e44bd9c1c2e75d90695b76e1
Kazuma Goto <kazuma.gotoh@jp.ricoh.com>
2024-02-19 10:05:03 +0900

    フォルダ整理

--- py/script.py
+++ py/script.py
@@ -45,7 +45,7 @@ if __name__ == '__main__':
         import os
         from pathlib import Path
         import utils
-        data_dir = Path(__file__).parent
+        data_dir = Path(__file__).parent.parent
         fpath = os.path.join(data_dir, "handinput.txt")
         utils.replace_stdin(fpath)
 
