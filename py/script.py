3e307ab538fbd4891a8ba178048d1e621bd6aac2
Kazuma Goto <kazuma.gotoh@jp.ricoh.com>
2024-02-22 08:03:29 +0900

    add comment

--- py/script.py
+++ py/script.py
@@ -2,6 +2,10 @@
 import numpy as np
 
 def resolve(N, K, A):
+    """
+    問題固有のメソッド。引数や戻り値は問題によって変える。
+    """
+
     MOD = 10 ** 9+7
 
     dp = [[0] * (K+1) for i in range(N+1)]
@@ -19,6 +23,10 @@ def resolve(N, K, A):
     return dp[N][K]
 
 def execute():
+    """
+    pytestでの呼び出しを想定したメソッドである。
+    """
+
     # read single value
     # N = int(input())
 
@@ -42,6 +50,8 @@ if __name__ == '__main__':
     import sys
 
     if len(sys.argv) > 1 and sys.argv[1] == "DEBUG":
+        # launch.jsonで第1引数にDEBUGを渡すように設定している
+        # ローカル実行環境の場合のみここを通る
         import os
         from pathlib import Path
         import utils
--- py/utils.py
+++ py/utils.py
@@ -3,6 +3,9 @@
 import os, sys, io
 
 def replace_stdin(fpath):
+    """
+    標準入力を指定されたファイルの内容に置き換える
+    """
     with open(fpath, 'r') as f:
         content = f.read()
     sys.stdin = io.StringIO(content)
